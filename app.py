1faac6fe4c35f9ccdebb351318402c805172da38
haoheiyo <46547049+haoheiyo@users.noreply.github.com>
2022-01-18 20:43:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
-
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory,Response
 import urllib.parse
 
 app = Flask(__name__)
@@ -8,15 +7,15 @@ app = Flask(__name__)
 
 @app.route('/plist', methods=['GET', 'POST'])
 def plist():
-    app_url=request.args.get("app_url")
-    with open('templates/ipa.plist','r') as f1:
+    app_url=urllib.parse.unquote(request.args.get("app_url"))
+    ts=app_url.split("/")[-1].split(".")[0]
+    with open('static/ipa.plist','r') as f1:
         data=f1.read()
-    with open('templates/ipa_new.plist','w') as f2:
+    with open('static/ipa_%s.plist'%ts,'w') as f2:
         f2.write(data.replace("app_url",app_url))
-    return send_from_directory('templates','ipa_new.plist')
+    return "https://360cec.b7.cm/static/ipa_%s.plist"%ts
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='localhost')
-
 
