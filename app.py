cb71de7549bbb75b9580e20c6f6eb4dc78847652
haoheiyo <46547049+haoheiyo@users.noreply.github.com>
2022-01-18 16:39:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask,request
+
+from flask import Flask, request, render_template, send_from_directory
 import urllib.parse
 
 app = Flask(__name__)
@@ -8,43 +9,14 @@ app = Flask(__name__)
 @app.route('/plist', methods=['GET', 'POST'])
 def plist():
     app_url=request.args.get("app_url")
-    plist_str = """
-
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-<key>items</key>
-<array>
-<dict>
-<key>assets</key>
-<array>
-<dict>
-<key>kind</key>
-<string>software-package</string>
-<key>url</key>
-<string>%s</string>
-</dict>
-</array>
-<key>metadata</key>
-<dict>
-<key>bundle-identifier</key>
-<string>com.cn.abctest</string>
-<key>bundle-version</key>
-<string>1.0</string>
-<key>kind</key>
-<string>software</string>
-<key>安装包_二狗</key>
-<string>abctest</string>
-</dict>
-</dict>
-</array>
-</dict>
-</plist>  
-""" % urllib.parse.unquote(app_url)
-    return plist_str
+    with open('templates/ipa.plist','r') as f1:
+        data=f1.read()
+    with open('templates/ipa_new.plist','w') as f2:
+        f2.write(data.replace("app_url",app_url))
+    return send_from_directory('templates','ipa_new.plist')
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='localhost')
+
 
